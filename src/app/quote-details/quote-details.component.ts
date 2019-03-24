e2600ddd8cf024881447636d97e15ecea39bf79c
adho <adhoadhigal>
2019-03-24 07:33:33 +0300

    Remove dist from gitignore

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -8,7 +8,7 @@ import {Quotes} from '../quotes';
   styleUrls: ['./quote-details.component.css']
 })
 export class QuoteDetailsComponent implements OnInit {
-
+  
   @Input() quote:Quotes;
   @Output() isComplete= new EventEmitter<boolean>();
 
