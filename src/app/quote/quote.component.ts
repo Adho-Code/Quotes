c15dbf1237a5ed5c85ace27b853a9155732e8dd0
adho <adhoadhigal>
2019-03-25 12:23:28 +0300

    modified style.css

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -9,11 +9,13 @@ import { Quote } from '../quotes';
 export class Quotecomponent implements OnInit {
    public show:boolean = false;
    public buttonName:any = 'show';
-   
+
      quotes = [
       new Quote(1,'Chuu Galgallo','Elizabeth Mwangi Robert','Be the change that you wish to see in the world',new Date(2019,3,11)),
       new Quote(1,'Peter Munya','Karungi Ochieng','No one can make you feel inferior without your consent.',new Date(2019,12.10)),
       new Quote(3,'Joseph Kariuki','Dinah Gitonga','It is never too late to be what you might have been',new Date(2019,4,9)),
+      new Quote(4,'Ken Maina','Lilian Waweru','Without music, life would be a mistake',new Date(2019,5,8)),
+    
     ]
 
     addNewQuote(quote){
